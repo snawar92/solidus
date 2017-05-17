3a567e6133dcda09ee81d022137584acca0915d6
Martin Meyerhoff <martin@stembolt.com>
2017-05-17 16:05:48 +0200

    Refactor redirecting on invalid order state
    In the frontend checkout process, there is two methods that handle
    validating the correct order state (or desired state):
    `#set_state_if_present` and `#ensure_valid_state`. Both redirect
    under certain conditions.
    
    This commit changes that behaviour such that:
    `#set_state_if_present` sets the current order's state to
    the desired state so that partials are rendered correctly.
    This method now also clears the parameter if the desired state is
    not a possible future state of the order.
    
    All redirects are moved to `#ensure_valid_state`. If any redirects happen, the
    method returns so that no double render errors are possible.

--- frontend/app/controllers/spree/checkout_controller.rb
+++ frontend/app/controllers/spree/checkout_controller.rb
@@ -108,12 +108,12 @@ module Spree
     end
 
     def ensure_valid_state
-      unless skip_state_validation?
-        if (params[:state] && !@order.has_checkout_step?(params[:state])) ||
-          (!params[:state] && !@order.has_checkout_step?(@order.state))
-          @order.state = 'cart'
-          redirect_to checkout_state_path(@order.checkout_steps.first)
-        end
+      # If we have no state parameter, we just transitioned from cart. Everything is fine.
+      return if params[:state].blank?
+
+      # If the current orders state has been set to something that not the current, but a future checkout state
+      if @order.can_go_to_state?(params[:state]) && !skip_state_validation
+        redirect_to checkout_state_path(@order.state) && return
       end
 
       # Fix for https://github.com/spree/spree/issues/4117
@@ -136,9 +136,10 @@ module Spree
     end
 
     def set_state_if_present
-      if params[:state]
-        redirect_to checkout_state_path(@order.state) if @order.can_go_to_state?(params[:state]) && !skip_state_validation?
+      if params[:state] && @order.has_checkout_step?(params[:state])
         @order.state = params[:state]
+      elsif !@order.has_checkout_step(params[:state]) && !skip_state_validation
+        params[:state] = nil
       end
     end
 
